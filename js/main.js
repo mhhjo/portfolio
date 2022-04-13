9651ab08fce168cf7557682385388a9c1bc7de19
mhhjo <102579010+mhhjo@users.noreply.github.com>
2022-04-13 18:02:38 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    var text_num = 0;
+    // function auto_moving(){
+    //     text_num++;
+    //     if(text_num>11){
+    //         text_num = 0;
+    //     }
+    //     $('.navbar-brand>span').animate({top:-10},200,function(){
+    //         $(this).animate({top:0},200)
+    //     })
+    // }
+})
